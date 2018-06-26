3436c338953fecaf3194efb94c6dc40b5a9794ee
devprog <jmloversg@gmail.com>
2018-06-26 23:35:43 +0900

    - 초기화

---
+++ Assets/Scripts/Character/AnimationState/AniStateController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniStateController
+{
+    AbstractAniState aniState = new AniIdle(enumAniState.Idle);
+    Animator anim;
+
+    // 생성자
+    public AniStateController(Animator animator)
+    {
+        anim = animator;
+        int clipindex = 0;
+        foreach(AnimationClip clip in anim.runtimeAnimatorController.animationClips)
+        {
+            if (clip.name.Contains("Attack"))
+            {
+                AnimationEvent evt = new AnimationEvent();
+                evt.intParameter = clipindex++;
+                evt.time = 0.2f;
+                evt.functionName = "OnEvent";
+                clip.AddEvent(evt);
+            }
+        }
+    }
+
+    public void SetAniState(AbstractAniState newAniState)
+    {
+        aniState = newAniState;
+        Play(aniState.currentAniState.ToString());
+    }
+
+    public void Update(float time)
+    {
+        aniState.Update(time, this);
+    }
+
+    private void Play(string animationname)
+    {
+        anim.Play(animationname);
+    }    
+
+    public void OnEvent(int parameter)
+    {
+        //Debug.Log("show:" + parameter);
+    }
+}
---
+++ Assets/Scripts/Character/PlayerAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+
+    AniStateController controller;
+
+    // Use this for initialization
+    void Start () {
+        Animator anim = gameObject.GetComponent<Animator>();        
+        controller = new AniStateController(anim);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Update(Time.deltaTime);
+    }
+
+    void OnEvent(int parameter)
+    {
+        controller.OnEvent(parameter);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Start");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision2" + gameObject.name);
+    }
+    
+}
+
+
+
+
+
