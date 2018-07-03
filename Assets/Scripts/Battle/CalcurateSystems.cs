74dc70d687a001ca98419c3a54518f70f2ace046
jmlovercrew <gantz0@naver.com>
2018-07-03 19:33:39 +0900

    - 계산 매니저 생성

---
+++ Assets/Scripts/Battle/CalcurateSystems.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// 
+public class CalcurateSystems
+{
+    private Queue<int> _queuePointData = new Queue<int>();
+
+
+    public int DequePoint(){
+        return 0;
+    }
+
+}
